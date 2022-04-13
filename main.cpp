abb9fa7716fe4223aabb628688e0af23639bdc49
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-04-13 15:12:20 +0900

    ウィンドウプロシージャ

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <Windows.h>
 
+//ウィンドウプロシージャ
+LRESULT WindowProc(HWND hwnd,UINT msg, WPARAM wparam, LPARAM lparam){
+	switch (msg){
+	//ウィンドウ破棄されたなら
+	case WM_DESTROY:
+		//OSに対してアプリ終了を通知
+		PostQuitMessage(0);
+		return 0;
+	}
+	
+	//メッセージ処理
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
 int WINAPI winMain(HINSTANCE,HINSTANCE,LPSTR,int){
 	
 	return 0;
