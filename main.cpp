d611aae154fee6440691b2dca32dd97ca4948b3b
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-04-13 18:08:01 +0900

    コマンドのフラッシュ

--- main.cpp
+++ main.cpp
@@ -235,6 +235,21 @@ int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int){
 		barrierDesc.Transition.StateAfter = D3D12_RESOURCE_STATE_PRESENT;
 		commandList->ResourceBarrier(1, &barrierDesc);
 
+
+		//命令のクローズ
+		result = commandList->Close();
+		assert(SUCCEEDED(result));
+
+		//コマンドリストの実行
+		ID3D12CommandList* commandLists[] = { commandList };
+		commandQueue->ExecuteCommandLists(1, commandLists);
+
+		//画面に表示するバッファをフリップ(裏表の入れ替え)
+		result = swapChain->Present(1, 0);
+		assert(SUCCEEDED(result));
+
+
+
 		//メッセージはあるか？
 		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
 			TranslateMessage(&msg);
