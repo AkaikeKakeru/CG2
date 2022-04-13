329c87065a1d9b16fc2c6ebb0f4c2bb8a31f3e23
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-04-13 17:12:59 +0900

    レンダ―ターゲットビュー

--- main.cpp
+++ main.cpp
@@ -171,6 +171,25 @@ int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int){
 	std::vector<ID3D12Resource*> backBuffers;
 	backBuffers.resize(swapChainDesc.BufferCount);
 
+
+	//スワップチェーンのすべてのバッファについて処理する
+	for (size_t i = 0; i < backBuffers.size(); i++) {
+		//スワップチェーンからバッファを取得
+		swapChain->GetBuffer((UINT)i, IID_PPV_ARGS(&backBuffers[i]));
+		//デスクリプタヒープのハンドルを取得
+		D3D12_CPU_DESCRIPTOR_HANDLE rtvHandle = rtvHeap->GetCPUDescriptorHandleForHeapStart();
+		//裏か表化でアドレスがズレる
+		rtvHandle.ptr += i * device->GetDescriptorHandleIncrementSize(rtvHeapDesc.Type);
+		//レンダ―ターゲットビューの設定
+		D3D12_RENDER_TARGET_VIEW_DESC rtvDesc{};
+		//シェーダーの計算結果をSRGBに変換して書き込む
+		rtvDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+		rtvDesc.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D;
+		//レンダ―ターゲットビューの生成
+		device->CreateRenderTargetView(backBuffers[i], &rtvDesc, rtvHandle);
+	}
+
+
 	//ゲームループ
 	while (true) {
 		//メッセージはあるか？
