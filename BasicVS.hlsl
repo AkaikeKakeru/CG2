48016cc08b0e8dbdc541935f3af24f10c22862bc
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-06-30 15:20:17 +0900

    行列をシェーダに移す

--- BasicVS.hlsl
+++ BasicVS.hlsl
@@ -3,7 +3,11 @@
 VSOutput main( float4 pos : POSITION, float2 uv : TEXCOORD )
 {
 	VSOutput output; //ピクセルシェーダ―に渡す値
-	output.svpos = pos;
+
+	////元々のコード
+	//output.svpos = pos;
+
+	output.svpos = mul(mat, pos); //座標に行列を乗算
 	output.uv = uv;
 	return output;
 }
