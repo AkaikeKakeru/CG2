444b041743ff36113c1a5a0eaeecf3eb213128d3
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-06-30 15:05:07 +0900

    誤述修正

--- main.cpp
+++ main.cpp
@@ -634,38 +634,38 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 #pragma endregion
 
-#pragma region constMapMaterial関連
-	
-	//ヒープ設定
-	D3D12_HEAP_PROPERTIES cbHeapProp{};
-	cbHeapProp.Type = D3D12_HEAP_TYPE_UPLOAD; // GPUへの転送用
-	//リソース設定
-	D3D12_RESOURCE_DESC cbResourceDesc{};
-	cbResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
-	cbResourceDesc.Width = (sizeof(ConstBufferDataMaterial ) + 0xff) & ~0xff; //256バイトアラインメント
-	cbResourceDesc.Height = 1;
-	cbResourceDesc.DepthOrArraySize = 1;
-	cbResourceDesc.MipLevels = 1;
-	cbResourceDesc.SampleDesc.Count = 1;
-	cbResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-	
-	ID3D12Resource* constBuffMaterial = nullptr;
-	//定数バッファの生成
-	result = device->CreateCommittedResource(
-		&cbHeapProp, //ヒープ設定
-		D3D12_HEAP_FLAG_NONE,
-		&cbResourceDesc, //リソース設定
-		D3D12_RESOURCE_STATE_GENERIC_READ,
-		nullptr,
-		IID_PPV_ARGS(&constBuffMaterial));
-	assert(SUCCEEDED(result));
-
-	//定数バッファのマッピング
-	ConstBufferDataMaterial* constMapMaterial = nullptr;
-	result = constBuffMaterial->Map(0, nullptr, (void**)&constMapMaterial); //マッピング
-	assert(SUCCEEDED(result));
-	
-#pragma endregion
+//#pragma region constMapMaterial関連
+//	
+//	//ヒープ設定
+//	D3D12_HEAP_PROPERTIES cbHeapProp{};
+//	cbHeapProp.Type = D3D12_HEAP_TYPE_UPLOAD; // GPUへの転送用
+//	//リソース設定
+//	D3D12_RESOURCE_DESC cbResourceDesc{};
+//	cbResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+//	cbResourceDesc.Width = (sizeof(ConstBufferDataMaterial ) + 0xff) & ~0xff; //256バイトアラインメント
+//	cbResourceDesc.Height = 1;
+//	cbResourceDesc.DepthOrArraySize = 1;
+//	cbResourceDesc.MipLevels = 1;
+//	cbResourceDesc.SampleDesc.Count = 1;
+//	cbResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+//	
+//	ID3D12Resource* constBuffMaterial = nullptr;
+//	//定数バッファの生成
+//	result = device->CreateCommittedResource(
+//		&cbHeapProp, //ヒープ設定
+//		D3D12_HEAP_FLAG_NONE,
+//		&cbResourceDesc, //リソース設定
+//		D3D12_RESOURCE_STATE_GENERIC_READ,
+//		nullptr,
+//		IID_PPV_ARGS(&constBuffMaterial));
+//	assert(SUCCEEDED(result));
+//
+//	//定数バッファのマッピング
+//	ConstBufferDataMaterial* constMapMaterial = nullptr;
+//	result = constBuffMaterial->Map(0, nullptr, (void**)&constMapMaterial); //マッピング
+//	assert(SUCCEEDED(result));
+//	
+//#pragma endregion
 
 #pragma region constMapTransfrom関連
 	
@@ -698,7 +698,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		//定数バッファのマッピング
 		ConstBufferDataMaterial* constMapTransform = nullptr;
-		result = constBuffMaterial->Map(0, nullptr, (void**)&constMapTransform); //マッピング
+		result = constBuffTransform->Map(0, nullptr, (void**)&constMapTransform); //マッピング
 		assert(SUCCEEDED(result));
 
 	// 値を書き込むと自動的に転送される
