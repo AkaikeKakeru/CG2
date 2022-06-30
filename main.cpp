79538287923e556ffc1f122597606d2ca8058ac6
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-06-30 15:01:44 +0900

    定数バッファのマッピング

--- main.cpp
+++ main.cpp
@@ -627,17 +627,22 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		XMMATRIX mat; //3D変換行列
 	};
 
-	ID3D12Resource* constBufferTransform = nullptr;
-	ConstBufferDataTransform* constMapTransform = nullptr;
+	/*ID3D12Resource* constBufferMaterial = nullptr;
+	ConstBufferDataTransform* constMapMaterial = nullptr;*/
+
+	
 
 #pragma endregion
+
+#pragma region constMapMaterial関連
+	
 	//ヒープ設定
 	D3D12_HEAP_PROPERTIES cbHeapProp{};
 	cbHeapProp.Type = D3D12_HEAP_TYPE_UPLOAD; // GPUへの転送用
 	//リソース設定
 	D3D12_RESOURCE_DESC cbResourceDesc{};
 	cbResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
-	cbResourceDesc.Width = (sizeof(ConstBufferDataTransform) + 0xff) & ~0xff; //256バイトアラインメント
+	cbResourceDesc.Width = (sizeof(ConstBufferDataMaterial ) + 0xff) & ~0xff; //256バイトアラインメント
 	cbResourceDesc.Height = 1;
 	cbResourceDesc.DepthOrArraySize = 1;
 	cbResourceDesc.MipLevels = 1;
@@ -652,18 +657,55 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		&cbResourceDesc, //リソース設定
 		D3D12_RESOURCE_STATE_GENERIC_READ,
 		nullptr,
-		IID_PPV_ARGS(&constBufferTransform));
+		IID_PPV_ARGS(&constBuffMaterial));
 	assert(SUCCEEDED(result));
 
-
-
-	////定数バッファのマッピング
+	//定数バッファのマッピング
 	ConstBufferDataMaterial* constMapMaterial = nullptr;
 	result = constBuffMaterial->Map(0, nullptr, (void**)&constMapMaterial); //マッピング
 	assert(SUCCEEDED(result));
+	
+#pragma endregion
+
+#pragma region constMapTransfrom関連
+	
+
+		//ヒープ設定
+		D3D12_HEAP_PROPERTIES cbHeapProp{};
+		cbHeapProp.Type = D3D12_HEAP_TYPE_UPLOAD; // GPUへの転送用
+		 //リソース設定
+		D3D12_RESOURCE_DESC cbResourceDesc{};
+		cbResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+		cbResourceDesc.Width = (sizeof(ConstBufferDataTransform) + 0xff) & ~0xff; //256バイトアラインメント
+		cbResourceDesc.Height = 1;
+		cbResourceDesc.DepthOrArraySize = 1;
+		cbResourceDesc.MipLevels = 1;
+		cbResourceDesc.SampleDesc.Count = 1;
+		cbResourceDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+
+
+		ID3D12Resource* constBuffTransform = nullptr;
+		//定数バッファの生成
+		result = device->CreateCommittedResource(
+			&cbHeapProp, //ヒープ設定
+			D3D12_HEAP_FLAG_NONE,
+			&cbResourceDesc, //リソース設定
+			D3D12_RESOURCE_STATE_GENERIC_READ,
+			nullptr,
+			IID_PPV_ARGS(&constBuffTransform));
+		assert(SUCCEEDED(result));
+
+		//定数バッファのマッピング
+		ConstBufferDataMaterial* constMapTransform = nullptr;
+		result = constBuffMaterial->Map(0, nullptr, (void**)&constMapTransform); //マッピング
+		assert(SUCCEEDED(result));
 
 	// 値を書き込むと自動的に転送される
 	constMapMaterial->color = XMFLOAT4(1.0f, 0.0f, 0.0f, 0.5f); //RGBAで半透明の赤
+
+	
+#pragma endregion
 
 
 
