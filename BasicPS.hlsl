1eb11f66b1ff3ef0c0ee16bdb0821514c44b2e65
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-07-11 09:35:10 +0900

    環境光の計算

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -8,9 +8,12 @@ float4 main(VSOutput input) : SV_TARGET
 	//右下奥 向きのライト
 	float3 light = normalize(float3(1,-1,1));
 
+	//diffuseを[0,1]の範囲にClampする
 	//光源へのベクトルと法線ベクトルの内積
-	float brightness = dot(-light, input.normal);
+	float diffuse = saturate(dot(-light, input.normal));
 
+	//アンビエント項を0.3として計算
+	float brightness = diffuse + 0.3f;
 
 
 	//輝度をRGBに代入して出力
