735fc2695a3bfbf5344ab89ee05f5daff29e9caf
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-07-11 11:54:54 +0900

    3Dオブジェクト型の構造体を定義

--- main.cpp
+++ main.cpp
@@ -29,6 +29,40 @@
 
 using namespace DirectX;
 
+//定数バッファ用データ構造体(マテリアル)
+struct ConstBufferDataMaterial
+{
+	XMFLOAT4 color; //色(RGBA)
+};
+
+//資料05-02で追加
+#pragma region 3D変換行列
+//定数バッファ用データ構造体(3D変換行列)
+struct ConstBufferDataTransform {
+	XMMATRIX mat; //3D変換行列
+};
+
+//3Dオブジェクト型
+struct Object3d
+{
+	//定数バッファ(行列用)
+	ID3D12Resource* constBuffTransform;
+
+	//定数バッファマップ(行列用)
+	ConstBufferDataTransform* constMapTransform;
+
+	//アフィン変換情報
+	XMFLOAT3 scale = { 1,1,1 };
+	XMFLOAT3 rotation = { 0,0,0 };
+	XMFLOAT3 position = { 0,0,0 };
+
+	//ワールド変換行列
+	XMMATRIX matWorld;
+
+	//親オブジェクトへのポインタ
+	Object3d* parent = nullptr;
+};
+
 //ウィンドウプロシージャ
 LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 	switch (msg) {
@@ -723,18 +757,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		IID_PPV_ARGS(&pipelineState));
 	assert(SUCCEEDED(result));
 
-	//定数バッファ用データ構造体(マテリアル)
-	struct ConstBufferDataMaterial
-	{
-		XMFLOAT4 color; //色(RGBA)
-	};
-
-	//資料05-02で追加
-#pragma region 3D変換行列
-	//定数バッファ用データ構造体(3D変換行列)
-	struct ConstBufferDataTransform {
-		XMMATRIX mat; //3D変換行列
-	};
+	
 
 	ID3D12Resource* constBuffMaterial = nullptr;
 
