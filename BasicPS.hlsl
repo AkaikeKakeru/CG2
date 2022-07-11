f4fdf3d0160538346cbd1cffdecf7d62a43ae154
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-07-11 09:30:49 +0900

    拡散反射光の計算

--- BasicPS.hlsl
+++ BasicPS.hlsl
@@ -5,5 +5,18 @@ SamplerState smp : register(s0); //0番スロットに設定されたサンプラー
 
 float4 main(VSOutput input) : SV_TARGET
 {
-	return float4(input.normal,1);//RGBをそれぞれの法線のXYZ、Aを1で出力
+	//右下奥 向きのライト
+	float3 light = normalize(float3(1,-1,1));
+
+	//光源へのベクトルと法線ベクトルの内積
+	float brightness = dot(-light, input.normal);
+
+
+
+	//輝度をRGBに代入して出力
+	return float4(brightness,brightness,brightness,1);
+
+
+	//RGBをそれぞれの法線のXYZ、Aを1で出力
+	//return float4(input.normal,1);
 }
