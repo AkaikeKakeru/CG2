65aac96a46e19ad7529b9e15f90bc1fed8e550fe
KakeruAkaike <k021g1137@g.neec.ac.jp>
2022-07-13 11:05:38 +0900

    定数バッファの初期化にあたる部分を、関数にまとめる

--- main.cpp
+++ main.cpp
@@ -63,6 +63,44 @@ struct Object3d
 	Object3d* parent = nullptr;
 };
 
+void InitializeObject3d(Object3d* object, ID3D12Device* device)
+{
+	HRESULT result;
+
+#pragma region constMapTransfrom関連
+
+	//ヒープ設定
+	D3D12_HEAP_PROPERTIES heapProp{};
+	heapProp.Type = D3D12_HEAP_TYPE_UPLOAD; // GPUへの転送用
+	//リソース設定
+	D3D12_RESOURCE_DESC resDesc{};
+	resDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	resDesc.Width = (sizeof(ConstBufferDataTransform) + 0xff) & ~0xff; //256バイトアラインメント
+	resDesc.Height = 1;
+	resDesc.DepthOrArraySize = 1;
+	resDesc.MipLevels = 1;
+	resDesc.SampleDesc.Count = 1;
+	resDesc.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+
+		//定数バッファの生成
+		result = device->CreateCommittedResource(
+			&heapProp, //ヒープ設定
+			D3D12_HEAP_FLAG_NONE,
+			&resDesc, //リソース設定
+			D3D12_RESOURCE_STATE_GENERIC_READ,
+			nullptr,
+			IID_PPV_ARGS(&object->constBuffTransform));
+		assert(SUCCEEDED(result));
+	
+		//定数バッファのマッピング
+		result = object->constBuffTransform->Map(0, nullptr,
+			(void**)&object->constMapTransform); //マッピング
+		assert(SUCCEEDED(result));
+
+#pragma endregion
+}
+
 //ウィンドウプロシージャ
 LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 	switch (msg) {
@@ -861,10 +899,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 //	assert(SUCCEEDED(result));
 #pragma endregion
 
+#pragma endregion
 	//単位行列を代入
 	//constMapTransform0->mat = XMMatrixIdentity();
 	//constMapTransform1->mat = XMMatrixIdentity();
-#pragma endregion
 
 #pragma region 単位行列で埋めた後
 #pragma region 平行投影行列の計算
